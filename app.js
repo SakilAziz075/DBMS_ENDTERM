2a96bba8bc3936861ec98109e2530a65420846f7
Ian2003 <sg725185@gmail.com>
2024-11-10 09:35:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors'); // Import cors
 const app = express();
 const doctorsRoute = require('./routes/doctors');
 const appointmentsRoute = require('./routes/appointments');
 const path = require('path');
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
