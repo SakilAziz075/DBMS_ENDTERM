b9baee59874d4b6ab1062b2aa603a3941306eed8
Ian2003 <sg725185@gmail.com>
2024-11-16 20:27:52 +0530

    Revert "Update appointments.js"
    This reverts commit 50056b92319a15f2dcad9e8133a5a929793e2cd3.

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const router = express.Router();
+const router= express.Router();
 const db = require('../db');
 
-router.post('/', async (req, res) => {
-    const { doctorID, patientName, appointmentDate, appointmentTime } = req.body;
+router.post('/' , async(req,res)=>{
+    const {doctorID , patientName , appointmentDate , appointmentTime } = req.body;
+    
+    try{
+        const result = await db.query
+        (
+            'INSERT INTO appointments (doctor_id, patient_name , appointmentDate , appointmentTime) VALUES(?,?,?,?)',
+            [doctorID, patientName , appointmentDate , appointmentTime]);
+            res.status(201).json({message:'appointment Booked sucessfully'});
+    }
 
-    try {
-        const result = await db.query(
-            'INSERT INTO appointments (doctor_id, patient_name, appointmentDate, appointmentTime) VALUES (?, ?, ?, ?)',
-            [doctorID, patientName, appointmentDate, appointmentTime]
-        );
-        res.status(201).json({ message: 'Appointment booked successfully' });
-    } catch (error) {
+    catch(error)
+    {
         console.error(error);
-        res.status(500).json({ message: 'Error booking appointment' });
+        res.status(500).json({message: 'Error booking appointment'});
     }
 });
 
